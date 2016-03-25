352f8654745d9bbba15ab7848bf745b94f8662e2
Diggsiii <rd3@me.com>
2016-03-25 10:41:20 -0500

    modified conversation.rb & create_conversations.rb

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,13 +1,16 @@
 class Conversation < ActiveRecord::Base
-  def change
-    create_table: conversations do |t|
-      t.integer :sender_id
-      t.integer :recipient_id
+  belongs_to :sender, :foreign_key => :sender_id, class_name: 'User'
+  belongs_to :recipient, :foreign_key => :recipient_id, class_name: 'User'
 
-      t.timestamps
-    end
+  has_many :messages, dependent: :destroy
 
-    add_index :conversations, :sender_id
-    add_index :conversations, :recipient_id
+  validates_uniqueness_of :sender_id, :scope => :recipient_id
+
+  scope :involving, -> (user) do
+    where("conversations.sender_id =? OR conversations.recipient_id =?",user.id,user.id)
+  end
+
+  scope :between, -> (sender_id,recipient_id) do
+    where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id,recipient_id, recipient_id, sender_id)
   end
 end
